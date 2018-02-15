3eb8ecdf682340990ee295b18be739732c3377ff
Stephen Gragg <graggsd@uab.edu>
2018-02-15 14:19:22 -0600

    Updated testing.

--- tests/testthat/test-remove_dupes_1way.R
+++ tests/testthat/test-remove_dupes_1way.R
@@ -1,26 +1,24 @@
 context("test-remove_dupes_1way.R")
 
-new <- data.frame(a = c("Apples", "Oranges", "Bananas"),
-                  b = c("Granny", "Florida", "Chiquita"),
-                  c = c("", "", ""),
-                  d = c("", "", ""),
-                  stringsAsFactors = FALSE)
-
-old <- data.frame(a = c("Apples", "Oranges"),
-                  b = c("Granny", "Florida"),
-                  c = c("Red", "Orange"),
-                  d = c("Green", "Orange"),
-                  stringsAsFactors = FALSE)
-
-expected_out <- data.frame(a = c("Apples", "Oranges", "Bananas"),
-                           b = c("Granny", "Florida", "Chiquita"),
-                           c = c("Red", "Orange", ""),
-                           d = c("Green", "Orange", ""),
-                           stringsAsFactors = FALSE)
-
-
-
 test_that("One-way duplicate removal works as expected", {
-    x <- find_dupes_1way(old, new, c("a", "b"))
-    expect_equal(remove_dupes_1way(x), expected_out)
+    new <- data.frame(a = c("Apples", "Oranges", "Bananas"),
+                      b = c("Granny", "Florida", "Chiquita"),
+                      c = c("", "", ""),
+                      d = c("", "", ""),
+                      stringsAsFactors = FALSE)
+
+    old <- data.frame(a = c("Apples", "Oranges"),
+                      b = c("Granny", "Florida"),
+                      c = c("Red", "Orange"),
+                      d = c("Green", "Orange"),
+                      stringsAsFactors = FALSE)
+
+    expected_out <- data.frame(a = c("Apples", "Oranges", "Apples", "Bananas"),
+                               b = c("Granny", "Florida", "Granny", "Chiquita"),
+                               c = c("Red", "Orange", "", ""),
+                               d = c("Green", "Orange", "", ""),
+                               stringsAsFactors = FALSE)
+
+    x <- find_dupes_1way(old, new, c("a", "b"), min_length = c(7, 1))
+    expect_equivalent(remove_dupes_1way(x), expected_out)
 })
