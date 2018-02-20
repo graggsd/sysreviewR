6eb44691f735c80d75aa292bf63676f59073b41f
Stephen Gragg <graggsd@uab.edu>
2018-02-20 13:28:24 -0600

    Added testing for update function.

--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -46,3 +46,53 @@ test_that("update_data protects short matching strings", {
                  expected_out)
 
 })
+
+
+test_that("update_data approximate matching works appropriately", {
+    expected_out <- data.frame(a = c("Apples", "Oranges", "Bananas"),
+                               b = c("Granny", "Florida", "Chiquita"),
+                               c = c("Red", "", ""),
+                               d = c("Green", "", ""),
+                               stringsAsFactors = FALSE)
+    update2 <- data.frame(a = c("Applesss", "Oranges"),
+                          b = c("Granny", "Florida"),
+                          c = c("Red", ""),
+                          d = c("Green", ""),
+                          stringsAsFactors = FALSE)
+    expected_out2 <- data.frame(a = c("Apples", "Oranges", "Bananas"),
+                               b = c("Granny", "Florida", "Chiquita"),
+                               c = c("Red", "", ""),
+                               d = c("Green", "", ""),
+                               stringsAsFactors = FALSE)
+
+    # Works for exact matches
+    expect_equal(update_data(empty = empty,
+                             populated = update,
+                             match_cols = c("a", "b"),
+                             min_length = 5,
+                             replace_cols = c("c", "d"),
+                             approx_match = TRUE,
+                             string_dist = 1),
+                 expected_out)
+
+    # Works for inexact matches
+    expect_equal(update_data(empty = empty,
+                             populated = update2,
+                             match_cols = c("a", "b"),
+                             min_length = 5,
+                             replace_cols = c("c", "d"),
+                             approx_match = TRUE,
+                             string_dist = 3),
+                 expected_out)
+
+    # Doesn't match when string distances is longer than assigned
+    expect_equal(update_data(empty = empty,
+                             populated = update2,
+                             match_cols = c("a", "b"),
+                             min_length = 5,
+                             replace_cols = c("c", "d"),
+                             string_dist = 1,
+                             approx_match = TRUE),
+                 expected_out2)
+
+})
