468f49c8ee60eb5091ef8eec36cc8b1ed3edb0e3
Stephen Gragg <graggsd@uab.edu>
2018-02-22 17:25:37 -0600

    Updated unit testing for dupes functions

--- tests/testthat/test-dupes.R
+++ tests/testthat/test-dupes.R
@@ -1,55 +1,83 @@
 context("test-dupes.R")
-test_dat <- form_mm_recs[1:5, c(1, 3, 4)]
 
-test_dat_2 <- rbind(test_dat, test_dat)
-test_dat_2[1, 1] <- paste0(test_dat_2[1, 1], "sadhfhkjkjhsdfhkjkhjsdf")
-test_dat_2[9, 1] <- sub("dexamethasone", "dexamethasign", test_dat_2[9, 1])
-test_dat_2[5, 1] <- "purple_rain"
-test_dat_2[10, 1] <- "purple_raining"
-test_dat_2_match <- add_matching_col(test_dat_2, "TITLE", TRUE)
+bat_ipsum1 <-
+    data.frame(TITLE = c("Accomplice? I'm gonna tell them the whole thing was your idea.",
+                         "I seek the means to fight injustice. To turn fear against those who prey on the fearful.",
+                         "Well, you see... I'm buying this hotel and setting some new rules about the pool area."),
+               AUTHOR = c("Batman", "Bruce", "Mr Wayne"),
+               DATABASE = rep("Batman Ipsum 1", 3),
+               stringsAsFactors = FALSE)
 
-test_that("fuzzy_matching is appropriately reponsive to different string distances", {
-
-    expect_equal(fuzzy_matching(test_dat_2_match, 10)$match_ID,
-                 c("m1", "m2", "m3", "m4", "m5",
-                   "m6", "m2", "m3", "m4", "m5"))
-    expect_equal(fuzzy_matching(test_dat_2_match, 1)$match_ID,
-                 c("m1", "m2", "m3", "m4", "m5",
-                   "m6", "m2", "m3", "m9", "m10"))
-
+test_that("approx_match == FALSE produces exact matching", {
+    btc1 <- data.frame(TITLE = c("Bread", "Tree", "Car"),
+                       AUTHOR = c("Fred", "Ted", "Ed"),
+                       stringsAsFactors = FALSE)
+    btc2 <- data.frame(TITLE = c("Bread", "Tree", "Car"),
+                       AUTHOR = c("Fred", "Ted", "Eddy"),
+                       stringsAsFactors = FALSE)
+    expected_out <- data.frame(UNIQUE_ID = 1:4,
+                               TITLE = c("Bread", "Tree", "Car", "Car"),
+                               AUTHOR = c("Fred", "Ted", "Ed", "Eddy"),
+                               stringsAsFactors = FALSE)
+    expect_equivalent(dupes_rm(dupes_find(x = rbind(btc1, btc2),
+                                          match_cols = c("TITLE", "AUTHOR"),
+                                          min_length = 5)),
+                      expected_out)
 })
 
-test_that("strict_matching works as expected", {
-    expect_equal(strict_matching(test_dat_2_match)$match_ID,
-                 c("m1", "m2", "m3", "m4", "m5",
-                   "m6", "m2", "m3", "m9", "m10"))
+
+test_that("approx_match == TRUE produces exact matching", {
+    bat_ipsum1 <-
+        data.frame(TITLE = c("Accomplice? I'm gonna tell them the whole thing was your idea.",
+                             "I seek the means to fight injustice. To turn fear against those who prey on the fearful.",
+                             "Car"),
+                   AUTHOR = c("Batman", "Bruce", "Ed"),
+                   stringsAsFactors = FALSE)
+
+    expect_equivalent(dupes_rm(dupes_find(x = rbind(bat_ipsum1, bat_ipsum1),
+                                          match_cols = c("TITLE", "AUTHOR"),
+                                          approx_match = FALSE,
+                                          string_dist = 3,
+                                          min_length = 1)),
+                      data.frame(UNIQUE_ID = 1:3,
+                                 bat_ipsum1,
+                                 stringsAsFactors = FALSE))
 })
 
-test_that("protect_min_length works for single columns", {
-    cols <- "TITLE"
-    expect_equal(sum(is.na(protect_min_length(test_dat_2_match,
-                                              cols, 1000)[, "match_ID"])),
-                 0)
-    expect_equal(sum(is.na(protect_min_length(test_dat_2_match,
-                                              cols, 1)[, "match_ID"])),
-                 10)
+test_that("min_length protects short strings", {
+    bat_ipsum1 <-
+        data.frame(TITLE = c("Accomplice? I'm gonna tell them the whole thing was your idea.",
+                             "I seek the means to fight injustice. To turn fear against those who prey on the fearful.",
+                             "Car"),
+                   AUTHOR = c("Batman", "Bruce", "Ed"),
+                   stringsAsFactors = FALSE)
+
+    expect_equivalent(dupes_rm(dupes_find(x = rbind(bat_ipsum1, bat_ipsum1),
+                                          match_cols = c("TITLE", "AUTHOR"),
+                                          approx_match = FALSE,
+                                          string_dist = 3,
+                                          min_length = 1)),
+                      data.frame(UNIQUE_ID = 1:3,
+                                 bat_ipsum1,
+                                 stringsAsFactors = FALSE))
+
+    expect_equivalent(dupes_rm(dupes_find(x = rbind(bat_ipsum1, bat_ipsum1),
+                                          match_cols = c("TITLE", "AUTHOR"),
+                                          approx_match = FALSE,
+                                          string_dist = 3,
+                                          min_length = 6)),
+                      dupes_rm(dupes_find(x = rbind(bat_ipsum1, bat_ipsum1),
+                                          match_cols = c("TITLE", "AUTHOR"),
+                                          approx_match = TRUE,
+                                          string_dist = 3,
+                                          min_length = 6)),
+                      data.frame(UNIQUE_ID = 1:4,
+                                 rbind(bat_ipsum1,
+                                       data.frame(TITLE = "Car",
+                                                  AUTHOR = "Ed",
+                                                  stringsAsFactors = FALSE)),
+                                 stringsAsFactors = FALSE))
 })
 
-test_that("protect_min_length works for multiple columns", {
-    cols <- c("TITLE", "FIRSTAUTHLN")
-    match_2_cols <- add_matching_col(test_dat, cols, TRUE)
-    expect_equal(sum(is.na(protect_min_length(match_2_cols,
-                                              cols, c(1, 1))[, "match_ID"])),
-                 5)
-    expect_equal(sum(is.na(protect_min_length(match_2_cols,
-                                              cols, c(1000, 1))[, "match_ID"])),
-                 0)
-    expect_equal(sum(is.na(protect_min_length(match_2_cols,
-                                              cols, c(1, 1000))[, "match_ID"])),
-                 0)
-    expect_equal(sum(is.na(protect_min_length(match_2_cols,
-                                              cols, c(1000, 1000))[, "match_ID"])),
-                 0)
-})
 
 
