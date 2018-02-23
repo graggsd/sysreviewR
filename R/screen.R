7f0328841af1e92bbc1f3bcb26e457acbf973f9b
Stephen Gragg <graggsd@uab.edu>
2018-02-22 19:15:40 -0600

    Updated screen function names. Added function screen_fulltxt_list to make screening lists for full text articles. Altered screen_read and screen_write functions to be compatible with both full-text and abstract screening lists.

--- R/screen.R
+++ R/screen.R
@@ -10,30 +10,67 @@
 #' screen abstracts
 #' @examples
 #' \dontrun{
-#' screening_list <- screen_abstract_list(form_mm_recs, c("you", "me"))
+#' screening_list <- screen_abst_list(form_mm_recs, c("you", "me"))
 #' }
 #' @export
-screen_abstract_list <- function(x,
-                                 reviewers,
-                                 title_col = "TITLE",
-                                 abstract_col = "ABSTRACT") {
-    UseMethod("screen_abstract_list")
+screen_abst_list <- function(x,
+                             reviewers,
+                             title_col = "TITLE",
+                             abstract_col = "ABSTRACT") {
+    UseMethod("screen_abst_list")
 }
 
 #' @export
-screen_abstract_list.default <- function(x, ...) {
+screen_abst_list.default <- function(x, ...) {
     stop("x must be of class data.frame")
 }
 
 #' @export
-screen_abstract_list.data.frame <- function(x,
-                                          reviewers,
-                                          title_col = "TITLE",
-                                          abstract_col = "ABSTRACT") {
+screen_abst_list.data.frame <- function(x,
+                                        reviewers,
+                                        title_col = "TITLE",
+                                        abstract_col = "ABSTRACT") {
 
     base_sheet <- x[, c("UNIQUE_ID", title_col, abstract_col)]
-    idx <- na.omit(match(c(title_col, abstract_col), colnames(base_sheet)))
-    colnames(base_sheet)[idx] <- c("TITLE", "ABSTRACT")
+    out <- lapply(reviewers,
+                  function(x) {
+                      base_sheet[, x] <- "not vetted"
+                      return(base_sheet)
+                  })
+    names(out) <- reviewers
+    return(out)
+}
+
+#' Create a list to screen full texts
+#'
+#' Creates a list of \code{data.frame}s to screen full texts
+#'
+#' @param x A set of publication records without duplicates
+#' @param reviewers Names of people that will screen full texts
+#' @param cols Columns from x to include in the full-text screeners
+#' @return A list of of \code{data.frame}s, formatted for each reviewer to
+#' screen abstracts
+#' @examples
+#' \dontrun{
+#' screening_list <- screen_abst_list(form_mm_recs, c("you", "me"))
+#' }
+#' @export
+screen_fulltxt_list <- function(x,
+                                reviewers,
+                                cols = "TITLE") {
+    UseMethod("screen_abst_list")
+}
+
+#' @export
+screen_fulltxt_list.default <- function(x, ...) {
+    stop("x must be of class data.frame")
+}
+
+#' @export
+screen_fulltxt_list.data.frame <- function(x,
+                                           reviewers,
+                                           cols = "TITLE") {
+    base_sheet <- x[, unique(c("UNIQUE_ID", cols))]
     out <- lapply(reviewers,
                   function(x) {
                       base_sheet[, x] <- "not vetted"
@@ -45,16 +82,16 @@ screen_abstract_list.data.frame <- function(x,
 
 #' Create .csv files for abstract screening
 #'
-#' Takes a the output of \code{screen_abstract_list} and creates a series of
+#' Takes a the output of \code{screen_abst_list} and creates a series of
 #' .csv files that may be distributed to the individuals that will screen
 #' abstracts
 #'
-#' @param x The list created by \code{screen_abstract_list}
+#' @param x The list created by \code{screen_abst_list}
 #' @param dir The desired directory into which .csv files will be placed
 #' @return NULL
 #' @examples
 #' \dontrun{
-#' screening_list <- screen_abstract_list(form_mm_recs, c("you", "me"))
+#' screening_list <- screen_abst_list(form_mm_recs, c("you", "me"))
 #' screen_write(screening_list, dir = "./")
 #' }
 #' @export
@@ -74,12 +111,12 @@ screen_write <- function(x, dir = "../intermediate_data/") {
 #' @param dir The directory containing complete screening forms
 #' @param ref_table The original set of records from which screening sheets
 #' were collected. Note: this must be unaltered from the point at which
-#' screening forms were first generated with \code{screen_abstract_list}.
+#' screening forms were first generated with \code{screen_abst_list}.
 #' @return A \code{data.frame} with the combined results of all screening
 #' sheets, plus the publication data from \code{ref_table}
 #' @examples
 #' \dontrun{
-#' screening_list <- screen_abstract_list(form_mm_recs, c("you", "me"))
+#' screening_list <- screen_abst_list(form_mm_recs, c("you", "me"))
 #' screen_write(screening_list, dir = "./")
 #' # Abstracts should be screened at this point
 #' combined_screening_forms <- screen_read("./", form_mm_recs)
@@ -87,16 +124,21 @@ screen_write <- function(x, dir = "../intermediate_data/") {
 #' @export
 screen_read <- function(dir = "../intermediate_data/", ref_table) {
     files <- paste0(dir, list.files(path = dir, pattern = "AbstScreener"))
+    names <- gsub("^.*AbstScreener_|\\.csv$", "", files)
+    keeper_cols <-
+        apply(expand.grid(names,
+                          c("OBTAINABLE", "INCLUDE", "EXCLUSION_RATIONALE")),
+              1, paste, collapse = "_")
+    keeper_cols <- c(names, "UNIQUE_ID", keeper_cols)
     abs_scr_list <-
         lapply(files,
                function(x) {
                    out <- read.csv(x, stringsAsFactors = FALSE)
-                   idx <- which(colnames(out) %in% c("TITLE", "ABSTRACT"))
-                   out <- out[, -idx]
+                   idx <- which(colnames(out) %in% keeper_cols)
+                   out <- out[, idx]
                })
     return(
         purrr::reduce(abs_scr_list, dplyr::full_join, by = "UNIQUE_ID") %>%
-            dplyr::full_join(ref_table, by = "UNIQUE_ID") %>%
-            dplyr::select(-SCREENING_ID)
+            dplyr::full_join(ref_table, by = "UNIQUE_ID")
     )
 }
